32aecdd3ecf576a1db478d868e5a24b6c18e8986
Semih Cetinguney <semihcetinguney@hotmail.com>
2018-05-07 18:18:28 +0200

    User registration

---
+++ Play4Match/Assets/Scripts/LoginUser.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginUser : MonoBehaviour {
+	private string email;
+	private string password;
+
+	public void setEmail(string _email)
+	{
+		email = _email;
+	}
+
+	public void setPassword(string _password)
+	{
+		password = _password;
+	}
+
+	public void LoginUserOnClick(){
+		Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+		auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+			if (task.IsCanceled) {
+				Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+				return;
+			}
+			if (task.IsFaulted) {
+				Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+				return;
+			}
+
+			Firebase.Auth.FirebaseUser newUser = task.Result;
+			Debug.LogFormat("User signed in successfully: {0} ({1})",
+				newUser.DisplayName, newUser.UserId);
+		});
+
+	}
+}
---
+++ Play4Match/Assets/Scripts/RegisterUser.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class RegisterUser : MonoBehaviour {
+
+    private string email;
+    private string password;
+
+    public void setEmail(string _email)
+    {
+        email = _email;
+    }
+
+    public void setPassword(string _password)
+    {
+        password = _password;
+    }
+
+    public void RegisterUserOnClick()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+				Debug.LogError("Something went wrong, please try again later " + task.Exception);
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("An account with this username already exists " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+				newUser.DisplayName.ToString(), newUser.UserId.ToString());
+        });
+    }
+}
