b9797762a14c3bd5c8d4fe162ea533c5da5dac60
jessewever <jwwever@hotmail.com>
2018-05-08 12:00:45 +0200

    Switchscene script
    script to switch the scene

---
+++ Play4Match/Assets/Code/SwitchScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour {
+
+    /// <summary>
+    /// to change scene give desired scene name as string
+    /// </summary>
+    /// <param name="sceneName">desired scene name</param>
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
