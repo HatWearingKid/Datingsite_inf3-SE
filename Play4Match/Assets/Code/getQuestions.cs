20a172811acdc4fc00c413e00f791ff3d2a4b9e5
Eelco Blaauw <eelco1990@gmail.com>
2018-05-14 15:50:39 +0200

    Added api to retrieve questions

--- Play4Match/Assets/Code/getQuestions.cs
+++ Play4Match/Assets/Code/getQuestions.cs
@@ -1,31 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Firebase;
-using Firebase.Database;
+using UnityEngine.Networking;
 
 public class getQuestions : MonoBehaviour {
 
+    Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
 	// Use this for initialization
 	void Start () {
-	  Firebase.Database.FirebaseDatabase dbInstance = Firebase.Database.FirebaseDatabase.DefaultInstance;
-	  dbInstance.GetReference("Users").GetValueAsync().ContinueWith(task => {
-				if (task.IsFaulted) {
-					// Handle the error...
-				}
-				else if (task.IsCompleted) {
-				  DataSnapshot snapshot = task.Result;
+        string userid = auth.CurrentUser.UserId;
 
-				  foreach ( DataSnapshot question in snapshot.Children)
-                  {
-					Debug.Log (question);
-				  }
-				}
-	  });
+        string url = "http://play4match.com/api/getq.php?id=" + userid;
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForRequest(www));
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
 	}
-	
-	// Update is called once per frame
-	//void Update () {
-	//	//
-	//}
 }
--- Website/api/getq.php
+++ Website/api/getq.php
@@ -3,13 +3,60 @@
 $id = isset($_GET['id']) ? $_GET['id'] : 'No id';
 
 //firebase
+// create curl resource 
+$ch = curl_init(); 
 
+// set url 
+curl_setopt($ch, CURLOPT_URL, "https://play4matc.firebaseio.com/.json"); 
+
+//return the transfer as a string 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+// $output contains the output string 
+$output = curl_exec($ch); 
+
+// close curl resource to free up system resources 
+curl_close($ch);      
+
+$dataArray = json_decode($output, true);
+		
 //kijken welke vragen de user al gedaan heeft
 
+$questionsDone = [];
+
+if(isset($dataArray['Users'][$id]))
+{
+	for($i = 0; $i < count($dataArray['Users'][$id]['Answered']); $i++)
+	{
+		array_push($questionsDone, $dataArray['Users'][$id]['Answered'][$i]);
+	}
+}
+
 // selecteer 5 vragen @random die de user nog niet gedaan heeft
+$newQuestions = [];
+
+$questionCount = 0;
+
+for($i = 0; $i < count($dataArray['Questions']); $i++)
+{
+	if(!isset($questionsDone[$i]))
+	{
+		array_push($newQuestions, $dataArray['Questions'][$i]);
+		array_push($questionsDone, $rand);
+		
+		$questionCount++;
+	}
+	
+	if($questionCount == 5)
+	{
+		break;
+	}
+}
 
 //return deze als json string
 
-echo('{"Response": "Hello world!", "id": "'.$id.'"}');
+echo(json_encode($newQuestions));
+
+//echo('{"Response": "Hello world!", "id": "'.$id.'"}');
 
 ?>
