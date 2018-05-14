8b69ac732e31f3deccdb5df956d32b5094eb75d1
Pekeltje <github@jordykroeze.com>
2018-05-14 14:53:20 +0200

    editProfile
    editProfile

---
+++ Play4Match/Assets/Code/editProfile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class editProfile : MonoBehaviour {
+
+    Firebase.Auth.FirebaseAuth auth;
+
+    // Use this for initialization
+    void Start () {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://play4matc.firebaseio.com/");
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void getData()
+    {
+        FirebaseDatabase.DefaultInstance
+        .GetReference("Question")
+        .GetValueAsync().ContinueWith(task => {
+            if (task.IsFaulted)
+            {
+                // Handle the error...
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                // Do something with snapshot...
+            }
+        });
+    }
+}
