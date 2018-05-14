9dccbc965c494a7f5e58827751a5265dd3a6a680
Semih Cetinguney <semihcetinguney@hotmail.com>
2018-05-14 16:34:58 +0200

    Layout main menu & layout profiel aanpassen

---
+++ Play4Match/Assets/Scripts/GetUserInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetUserInfo : MonoBehaviour {
+	Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+	public void GetData(){
+		Debug.Log (auth.CurrentUser.UserId);
+	}
+}
