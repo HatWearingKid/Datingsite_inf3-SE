2fa88316eabac41a91670a6110f44988db135d78
jessewever <jwwever@hotmail.com>
2018-05-17 09:28:04 +0200

    aanpassingen in vragen object

--- Play4Match/Assets/Code/getQuestions.cs
+++ Play4Match/Assets/Code/getQuestions.cs
@@ -5,15 +5,15 @@ using UnityEngine.Networking;
 
 public class getQuestions : MonoBehaviour {
 
-
+    WWW www;
 
 	// Use this for initialization
 	void Start () {
         Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
-        string userid = auth.CurrentUser.UserId;
+        string userid = "xh4S3DibGraTqCn8HascIIvdFR02";//auth.CurrentUser.UserId; //forceert nu eelco's account anders moet ik elke keer inloggen om te testen
 
         string url = "http://play4match.com/api/getq.php?id=" + userid;
-        WWW www = new WWW(url);
+        www = new WWW(url);
         StartCoroutine(WaitForRequest(www));
     }
 
@@ -36,4 +36,13 @@ public class getQuestions : MonoBehaviour {
     void Update () {
 
 	}
+    public GameObject questiontext;
+    
+    public void ShowQuestion(int QuestionNumber)
+    {
+        //make question apear on screen
+        questiontext = GameObject.Find("Question").GetComponent<Text>();
+        Debug.Log("question number: " + QuestionNumber);
+        Debug.Log(www.text[QuestionNumber]);
+    }
 }
