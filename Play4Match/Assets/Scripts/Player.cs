294305209ef8070deb4fd23a4f36148178c676c2
Semih Cetinguney <semihcetinguney@hotmail.com>
2018-05-18 10:49:58 +0200

    User wordt nu ge-insert in de database

---
+++ Play4Match/Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public string playerName;
+
+	public string SaveToString()
+	{
+		return JsonUtility.ToJson(this);
+	}
+}
