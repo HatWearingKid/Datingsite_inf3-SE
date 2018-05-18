2774a4ad32cd3c205a1d852721e87143b9fa339c
Semih Cetinguney <semihcetinguney@hotmail.com>
2018-05-18 12:33:38 +0200

    Comments toegevoegd

--- Play4Match/Assets/Scripts/RegisterUser.cs
+++ Play4Match/Assets/Scripts/RegisterUser.cs
@@ -5,8 +5,11 @@ using Firebase.Database;
 using Firebase.Unity.Editor;
 using System.Collections.Generic;
 
+/// <summary>
+/// Register user. A class used to register a user
+/// </summary>
 public class RegisterUser : MonoBehaviour {
-
+	// Initialize required variables
     private string email;
     private string password;
 	private string confirmPassword;
@@ -14,6 +17,7 @@ public class RegisterUser : MonoBehaviour {
 	Firebase.Auth.FirebaseUser user;
 	DatabaseReference reference;
 	Toast toast = new Toast();
+
 	// Initialise player to insert into the DB
 	Player player = new Player();
 	Liked liked = new Liked();
@@ -31,24 +35,30 @@ public class RegisterUser : MonoBehaviour {
 		user = auth.CurrentUser;
 	}
 		
+	// Setter method for the emial
     public void setEmail(string _email)
     {
         email = _email;
     }
 
+	// Setter method for the password
     public void setPassword(string _password)
     {
         password = _password;
     }
 
+	// Setter method for the confirmation password
 	public void setConfirmationPassword(string _confirmPassword)
 	{
 		confirmPassword = _confirmPassword;
 	}
 
+	// Method to register the user
     public void Register()
     {
+		// Check if both passwords match
 		if (password.Equals (confirmPassword)) {
+			// A method from Firebase to register the user asynchrone
 			auth.CreateUserWithEmailAndPasswordAsync (email, password).ContinueWith (task => {
 				if (task.IsCanceled) {
 					toast.MyShowToastMethod (task.Exception.InnerExceptions [0].Message);
@@ -61,17 +71,24 @@ public class RegisterUser : MonoBehaviour {
 
 				// Firebase user has been created.
 				user = task.Result;
+				// Ass the user to the database
 				AddUser(user.UserId);
+				// Send an email to the user
 				SendEmail ();
-				//auth.SignOut ();
+				// Log the user out (Firebase automatically logs the user in after registration)
+				auth.SignOut ();
 			});
 		} else {
+			// Show toast message if passwords do not match
 			toast.MyShowToastMethod ("The passwords do not match.");
 		}
     }
 
+	// Method for sending verification email 
 	public void SendEmail(){
+		// Check if there's a user to send a mail to
 		if (user != null) {
+			// A Firebase method to send a verification email asynchrone
 			user.SendEmailVerificationAsync ().ContinueWith (task => {
 				if (task.IsCanceled) {
 					Debug.LogError (task.Exception.InnerExceptions [0].Message);
@@ -82,13 +99,16 @@ public class RegisterUser : MonoBehaviour {
 					return;
 				}
 
+				// Toast message when succeed
 				toast.MyShowToastMethod ("A verification email has been sent, please activate your account.");
 			});
 		} else {
+			// Toast message when there's no user to send a mail to
 			toast.MyShowToastMethod ("Could not send email, something went wrong.");
 		}
 	}
 
+	// Method to add a user to the database
     public void AddUser(string userId) {
 		string jsonPlayer = JsonUtility.ToJson(player);
 		string jsonLiked = JsonUtility.ToJson(liked);
