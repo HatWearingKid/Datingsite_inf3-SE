49d8e5e9efc00c3a98a747a8cc707f3ae96d9e41
Pekeltje <github@jordykroeze.com>
2018-05-18 10:50:49 +0200

    Chat word ingevoerd
    Chat word ingevoerd

---
+++ Play4Match/Assets/Scripts/chatTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class chatTest : MonoBehaviour {
+
+    public DatabaseReference reference;
+
+    void Start () {
+        Debug.Log("Start");
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://play4matc.firebaseio.com/");
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        Debug.Log("Voor sendMessage aanroepen");
+        sendMessage("From","To","Content");
+        Debug.Log("Na sendMessage aanroepen");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void sendMessage(string from, string to, string content)
+    {
+        Debug.Log("Begin sendMessage");
+        chatMessage Message = new chatMessage(from, to, content);
+        string json = JsonUtility.ToJson(Message);
+
+        Debug.Log(json);
+
+        reference.Child("Chat").Child("userID").SetRawJsonValueAsync(json); // userID vervangen met het daadwerkelijke userID van de gebruiker
+        Debug.Log("Einde sendMessage");
+        
+    }
+
+    
+}
+
+public class chatMessage
+{
+    public string from;
+    public string to;
+    public string content;
+    public string date;
+
+    public chatMessage(string from, string to, string content)
+    {
+        Debug.Log("Begin chatMessage");
+        this.from = from;
+        this.to = to;
+        this.content = content;
+        this.date = System.DateTime.UtcNow.ToString();
+        Debug.Log("Einde chatMessage");
+    }
+}
+
