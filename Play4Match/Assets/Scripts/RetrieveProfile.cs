c9118fee24b3c09738423bfb374323a91559454f
Semih Cetinguney <semihcetinguney@hotmail.com>
2018-05-21 16:03:17 +0200

    Ophalen van user profile aangepast

--- Play4Match/Assets/Scripts/RetrieveProfile.cs
+++ Play4Match/Assets/Scripts/RetrieveProfile.cs
@@ -9,23 +9,21 @@ using Firebase.Unity.Editor;
 /// <summary>
 /// Retrieve and edit profile. Class to retrieve data and edit data of a user
 /// </summary>
-public class RetrieveAndEditProfile : MonoBehaviour {
+public class RetrieveProfile : MonoBehaviour {
 	// Initialize required variables
 	Firebase.Auth.FirebaseAuth auth;
 	Firebase.Auth.FirebaseUser user;
 	string userID;
 	IDictionary dictUser;
 	Toast toast = new Toast();
+	WWW www;
 
 	void Start () {
 		auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
 		user = auth.CurrentUser;
 		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://play4matc.firebaseio.com/");
 		DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
-	}
-
-	public void SetDisplayName(InputField input){
-		input.text = dictUser["displayName"].ToString();
+		GetProfile ();
 	}
 
 	public void SetName(InputField input){
@@ -37,21 +35,32 @@ public class RetrieveAndEditProfile : MonoBehaviour {
 	}
 
 	public void SetGender(InputField input){
-		input.text = dictUser["Gender"].ToString();
+		input.text = dictUser["gender"].ToString();
 	}
 
 	public void SetCountry(InputField input){
 		input.text = dictUser["Country"].ToString();
 	}
 
+	public void SetImage(Button button){
+		if(user.PhotoUrl != null){
+			GetImage ();
+			button.image.sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0));
+		}
+	}
+
+	public IEnumerator GetImage(){
+		if(user.PhotoUrl != null){
+			www = new WWW(user.PhotoUrl.ToString());
+			yield return www;
+		}
+	}
+
 	// Method to retrieve the user data
-	public void RetrieveProfile(){
+	public void GetProfile(){
 		// Check if user is logged in
 		if (user != null) {
 			userID = user.UserId;
-			Debug.Log (userID);
-		} else {
-			Debug.Log ("User not logged in.");
 		}
 
 		// Firebase method to make connection with the database and get the user's information
