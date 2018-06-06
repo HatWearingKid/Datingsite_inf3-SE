5bef922f8b2d236a25b9711873764e03f9de70b6
HatWearingKid <HatWearingKid@users.noreply.github.com>
2018-06-06 13:13:20 +0200

    Added soundSettings

---
+++ Play4Match/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour {
+
+	private AudioSource source;
+	
+	public AudioClip sliderSound;
+	public AudioClip muteSound;
+	public AudioClip matchSound;
+	
+	public bool mute;
+	
+	// Use this for initialization
+	void Start () {
+		mute = false;
+		source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+		{
+			source.Play();
+		}
+	}
+	
+	public void SetMute()
+	{
+		mute = !mute;
+		source.mute = mute;
+	}
+	
+	public void SetVolume(float volume)
+	{
+		source.volume = volume;
+	}
+		
+	
+	public void PlaySound(string soundName)
+	{
+		switch(soundName)
+		{
+			case "sliderSound":
+				source.clip = sliderSound;
+				break;
+			case "muteSound":
+				source.clip = muteSound;
+				break;
+			case "matchSound":
+				source.clip = matchSound;
+				break;
+			default: 
+				source.clip = sliderSound;
+				break;
+		}
+		
+	}
+}
+
