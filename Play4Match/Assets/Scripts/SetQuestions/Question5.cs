9809a237d2b203f048a7b7a58a641aa16297bfad
Semih Cetinguney <semihcetinguney@hotmail.com>
2018-06-14 14:18:51 +0200

    Slider toegevoegd aan startlevel distance field

---
+++ Play4Match/Assets/Scripts/SetQuestions/Question5.cs
@@ -0,0 +1,45 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Question5 : MonoBehaviour
+{
+
+    Firebase.Auth.FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser user;
+    DatabaseReference reference;
+
+    public Slider slider;
+
+    public void insertAnswer()
+    {
+        float distance;
+
+        if (slider.value == 5)
+        {
+            distance = 0;
+        }
+        else
+        {
+            distance = slider.value * 25;
+        }
+
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://play4matc.firebaseio.com/");
+
+        //connect to firebase and get userid
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        //string userId = "TestGebruiker";
+        //string userId = auth.CurrentUser.UserId;
+
+        reference.Child("Users").Child("Bs8KA32ZjRdIFCbgBkkZjUVvi753").Child("Distance").SetValueAsync(distance);
+    }
+}
