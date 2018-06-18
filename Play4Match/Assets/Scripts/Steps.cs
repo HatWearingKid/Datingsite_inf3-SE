bd13fed75f62742d49734f9ade89f817f1801801
jessewever <jwwever@hotmail.com>
2018-06-18 14:11:05 +0200

    camera kan (als het goed is) niet gesleept worden als popup open is

--- Play4Match/Assets/Scripts/Steps.cs
+++ Play4Match/Assets/Scripts/Steps.cs
@@ -123,13 +123,13 @@ public class Steps : MonoBehaviour {
             else if(touch.phase == TouchPhase.Moved)
             {
                 //if there is no popup over the level or if the camera is not locked
-                if (!movecamera && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+                if (!movecamera && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject() || Input.touchCount > 0 && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
                 {
                     //move camera according to finger movement
                     float deltaZ = initTouch.position.y - touch.position.y;
                     movZ = deltaZ * Time.deltaTime * moveSpeed * direction;
                     movZ = movZ + cameraPos.transform.position.z;
-                    if(movZ >= -44 && movZ <= maxCameraPlace && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+                    if(movZ >= -44 && movZ <= maxCameraPlace)
                     {
                         cameraPos.transform.position = new Vector3(cameraStand.x, 1481, movZ);
                     }
